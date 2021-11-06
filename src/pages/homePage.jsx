5c4e1fc560521df5b4e7f1b10bbcdc92097db9be
cartory <xXcartoryXx@gmail.com>
2021-11-05 22:01:05 -0400

    update homePage

--- src/pages/homePage.jsx
+++ src/pages/homePage.jsx
@@ -2,6 +2,7 @@ import logo from '../logo.svg'
 import { Routes, Route, useNavigate } from 'react-router-dom'
 
 import {
+	Box,
 	Grid,
 	AppBar,
 	Toolbar,
@@ -16,30 +17,36 @@ import { SettingsPage } from './auth/settingsPage'
 export const HomePage = () => {
 	const navigate = useNavigate()
 
-	return <>
-		<CssBaseline />
-		<AppBar elevation={1} position={'static'}>
-			<Toolbar style={{ minHeight: '56px' }}>
-				<Grid container direction="row" alignItems="center">
-					<Grid item md={10}>
-						<IconButton
-							children={<img src={logo} alt={logo} height={50} />}
-							onClick={() => navigate('/home')}
-						/>
+	return (
+		<Box sx={{ display: 'flex' }}>
+			<CssBaseline />
+			<AppBar
+				elevation={1}
+				position="fixed"
+				sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 }}
+			>
+				<Toolbar style={{ minHeight: '56px' }}>
+					<Grid container direction="row" alignItems="center">
+						<Grid item md={10}>
+							<IconButton
+								children={<img src={logo} alt={logo} height={50} />}
+								onClick={() => navigate('/home')}
+							/>
+						</Grid>
+						<Grid item md={1} sm={1}>
+							<h4 style={{ textAlign: 'end' }}>username</h4>
+						</Grid>
+						<Grid item md={1} sm={1}>
+							<img src={logo} alt={logo} height={40} style={{ borderRadius: 100 }} />
+						</Grid>
 					</Grid>
-					<Grid item md={1} sm={1}>
-						<h4 style={{ textAlign: 'end' }}>username</h4>
-					</Grid>
-					<Grid item md={1} sm={1}>
-						<img src={logo} alt={logo} height={40} style={{ borderRadius: 100 }} />
-					</Grid>
-				</Grid>
-			</Toolbar>
-		</AppBar>
-		<Routes>
-			<Route path="home" element={<ListPage />} />
-			<Route path="settings" element={<SettingsPage />} />
-			<Route path="detail/:id" element={<DetailPage />} />
-		</Routes>
-	</>
+				</Toolbar>
+			</AppBar>
+			<Routes>
+				<Route path="home" element={<ListPage />} />
+				<Route path="settings" element={<SettingsPage />} />
+				<Route path="detail/:id" element={<DetailPage />} />
+			</Routes>
+		</Box>
+	)
 } 
