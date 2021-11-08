8ac7c4d136847f58d153fa9da5f96c5854de90f4
cartory <xXcartoryXx@gmail.com>
2021-11-08 01:03:06 -0400

    update homePage > showUserData

--- src/pages/homePage.jsx
+++ src/pages/homePage.jsx
@@ -4,11 +4,16 @@ import { Routes, Route, useNavigate } from 'react-router-dom'
 import {
 	Box,
 	Grid,
+	Avatar,
 	AppBar,
 	Toolbar,
 	IconButton,
 	CssBaseline,
+	AvatarGroup,
 } from '@mui/material'
+
+import { getAuth } from 'firebase/auth'
+import { Logout } from '@mui/icons-material'
 
 import { ListPage } from './antiques/listPage'
 import { DetailPage } from './antiques/detailPage'
@@ -16,6 +21,11 @@ import { SettingsPage } from './auth/settingsPage'
 
 export const HomePage = () => {
 	const navigate = useNavigate()
+
+	const rawUser = sessionStorage.getItem('user')
+	!rawUser && navigate('/', { replace: true })
+
+	const user = JSON.parse(rawUser)
 
 	return (
 		<Box sx={{ display: 'flex' }}>
@@ -34,10 +44,22 @@ export const HomePage = () => {
 							/>
 						</Grid>
 						<Grid item md={1} sm={1}>
-							<h4 style={{ textAlign: 'end' }}>username</h4>
+							<h4 style={{ textAlign: 'end', paddingRight: 10 }}>{user.name}</h4>
 						</Grid>
 						<Grid item md={1} sm={1}>
-							<img src={logo} alt={logo} height={40} style={{ borderRadius: 100 }} />
+							<AvatarGroup max={2}>
+								<Avatar alt={user.name} src={user.photoUrl} sx={{ height: 50, width: 50 }} />
+								<IconButton
+									title='logOut'
+									children={[<Logout color='secondary' key='logout' />]}
+									onClick={async () => {
+										let auth = getAuth()
+										await auth?.signOut()
+										sessionStorage.clear()
+										navigate('/', { replace: true })
+									}}
+								/>
+							</AvatarGroup>
 						</Grid>
 					</Grid>
 				</Toolbar>
