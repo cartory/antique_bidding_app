04af841d0e06dde0fb78919d35f29cf77e58a37b
cartory <xXcartoryXx@gmail.com>
2021-11-07 22:27:22 -0400

    update useFetch > debounce, useSocket

--- src/hooks/useFetch.jsx
+++ src/hooks/useFetch.jsx
@@ -1,17 +1,44 @@
+import io from 'socket.io-client'
 import { useState, useEffect } from 'react'
 
-export const useFetchAPI = ({ endpoint = "", query = {} }) => {
+const debounce = (fn, milliSeconds = 0) => {
+	let timeout
+	return function () {
+		var context = this;
+		var args = arguments;
+
+		clearTimeout(timeout)
+		timeout = setTimeout(() => {
+			fn.apply(context, args)
+		}, milliSeconds)
+	}
+}
+
+export const useFetchAPI = ({ endpoint = "", query = {}, delayInMilliseconds = 0 }) => {
 	const [data, setData] = useState([])
 
 	Object.keys(query).forEach((k, index) => {
 		endpoint += (index === 0 ? '?' : '&') + `${k}=${query[k]}`
 	})
+
 	endpoint = `${process.env.REACT_APP_API}/api/${endpoint}`
+
 	useEffect(() => {
-		fetch(endpoint)
+		debounce(fetch(endpoint)
 			.then(async res => setData(await res.json()))
-			.catch(err => console.error(err))
-	}, [endpoint])
+			.catch(err => console.error(err)),
+			delayInMilliseconds,
+		);
+	}, [endpoint, delayInMilliseconds])
 
 	return [data, setData]
+}
+
+export const useSocket = () => {
+	try {
+		return io(process.env.REACT_APP_API, { transports: ['websocket'] })
+	} catch (err) {
+		console.error(err);
+	}
+	return null
 }
